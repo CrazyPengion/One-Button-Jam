eba8ade825ed06fbf1b149c51a0c9148752a5f1d
CrazyPengion <118538266+CrazyPengion@users.noreply.github.com>
2026-07-07 12:19:26 +0200

    +Web compatibility, main structure

--- One Button Jam/main.cpp
+++ One Button Jam/main.cpp
@@ -12,4 +12,52 @@
 * There could be one central main goal such as "Save X" / "Get your revenge" / etc.
 
 * Created by CrazyPengion, July of 2026
+
+* Gemini was primarily used for web compartablity.
 */
+
+#include "raylib.h"
+
+/// If we compile for web later, we need this header
+#if defined(PLATFORM_WEB)
+#include <emscripten.h>
+#endif
+
+void UpdateAndDrawFrame();
+void UpdateScreen();
+
+// main is only used for stuff to launch ONCE at start up - NO LOOPS
+int main()
+{
+    InitWindow(1280, 720, "Tap Wizard"); //720p, should fit 1080p monitors without fullscreen well (as well as old ones with full screen
+
+#if defined(PLATFORM_WEB) // if on web => let browser set FPS and run (monitor native)
+    emscripten_set_main_loop(UpdateAndDrawFrame, 0, 1);
+#else // if not on web => run this
+    SetTargetFPS(60);
+    while (!WindowShouldClose())
+    {
+        UpdateAndDrawFrame();
+    }
+#endif
+    /// UnloadTexture(playerSprite); /// Clean up assets
+    /// CloseAudioDevice();
+    CloseWindow();
+    return 0;
+}
+
+// everything outside of main (gameplay loops) - this verion's main(){} ------------------------------------------------------------------
+void UpdateAndDrawFrame()
+{
+    UpdateScreen();
+}
+
+void UpdateScreen()
+{
+    BeginDrawing();
+
+    ClearBackground(WHITE);
+    DrawText("test . t", 190, 200, 20, BLACK);
+
+    EndDrawing();
+}
