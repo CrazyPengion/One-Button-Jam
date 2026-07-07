ffac6d60eff1c8c0651cc7ee99d679d18ee6c65f
CrazyPengion <118538266+CrazyPengion@users.noreply.github.com>
2026-07-07 10:54:16 +0200

    Game Idea

---
+++ One Button Jam/main.cpp
@@ -0,0 +1,15 @@
+/* TAP WIZARD
+
+* Premise:
+* This game is made for the One Button Jam 2026 (by Clyde) on Itch.io. 
+* As the name says: only one button can be used as input (mouse movement / etc is not allowed, aside in menus).
+
+* Initial idea:
+* "Tap Wizard" / "Tap Wizards" (not as fitting as no multiplayer): a game where you can cast different spells to attack
+* mobs / enemies. To activate spells you need to do different combinations of tapping or holding your button (key).
+* You can collect / learn new spells (either by randomly trying stuff / upgrading your level / picking or buying them
+* as a reward for money/xp you got from eliminating mobs.
+* There could be one central main goal such as "Save X" / "Get your revenge" / etc.
+
+* Created by CrazyPengion, July of 2026
+*/
